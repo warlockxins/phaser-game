7b25ff9aedd99c7a29ac513cee872235f7ea2c7e
Alexander Semionov <warlockxins@gmail.com>
2019-02-21 22:15:54 +0100

    basic atlas check

--- src/js/app.js
+++ src/js/app.js
@@ -18,29 +18,45 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.setBaseURL("http://labs.phaser.io");
+    // this.load.setBaseURL("http://labs.phaser.io");
 
-    this.load.image("sky", "assets/skies/space3.png");
-    this.load.image("logo", "assets/sprites/phaser3-logo.png");
-    this.load.image("red", "assets/particles/red.png");
+    // this.load.image("sky", "assets/skies/space3.png");
+    // this.load.image("logo", "assets/sprites/phaser3-logo.png");
+    // this.load.image("red", "assets/particles/red.png");
+
+    this.load.atlas(
+        "atlas",
+        "assets/texturepacker_test.png",
+        "assets/texturepacker_test.json"
+    );
 }
 
 function create() {
-    this.add.image(400, 300, "sky");
+    // this.add.image(400, 300, "sky");
 
-    var particles = this.add.particles("red");
+    // var particles = this.add.particles("red");
 
-    var emitter = particles.createEmitter({
-        speed: 10,
-        scale: { start: 1, end: 0 },
-        blendMode: "ADD"
-    });
+    // var emitter = particles.createEmitter({
+    //     speed: 10,
+    //     scale: { start: 1, end: 0 },
+    //     blendMode: "ADD"
+    // });
 
-    var logo = this.physics.add.image(400, 100, "logo");
+    // var logo = this.physics.add.image(400, 100, "logo");
 
-    logo.setVelocity(10, 300);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+    // logo.setVelocity(10, 300);
+    // logo.setBounce(1, 1);
+    // logo.setCollideWorldBounds(true);
 
-    emitter.startFollow(logo);
+    // emitter.startFollow(logo);
+
+    const chick = this.add.sprite(64, 64, "atlas", "budbrain_chick.png");
+
+    const cop = this.add.sprite(600, 64, "atlas", "ladycop.png");
+
+    const robot = this.add.sprite(50, 300, "atlas", "robot.png");
+
+    const car = this.add.sprite(100, 400, "atlas", "supercars_parsec.png");
+
+    const mech = this.add.sprite(250, 100, "atlas", "titan_mech.png");
 }
