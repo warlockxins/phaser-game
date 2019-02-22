19b87319f9dab65b522b9f90298d46dd037ec9cf
Alexander Semionov <warlockxins@gmail.com>
2019-02-22 22:22:28 +0100

    add simple animation, removed not needed media

--- src/js/app.js
+++ src/js/app.js
@@ -11,52 +11,44 @@ var config = {
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
 var game = new Phaser.Game(config);
 
 function preload() {
-    // this.load.setBaseURL("http://labs.phaser.io");
-
-    // this.load.image("sky", "assets/skies/space3.png");
-    // this.load.image("logo", "assets/sprites/phaser3-logo.png");
-    // this.load.image("red", "assets/particles/red.png");
-
-    this.load.atlas(
-        "atlas",
-        "assets/texturepacker_test.png",
-        "assets/texturepacker_test.json"
-    );
+    this.load.atlas("slimeg", "assets/slimeg.png", "assets/slimeg.json");
 }
 
 function create() {
-    // this.add.image(400, 300, "sky");
+    addSlimeAnimation(this);
+}
 
-    // var particles = this.add.particles("red");
+const addSlimeAnimation = scene => {
+    scene.slime = scene.add.sprite(80, 400, "slimeg", "slime1.png");
+    const frameNames = scene.anims.generateFrameNames("slimeg", {
+        start: 1,
+        end: 5,
+        zeroPad: 1,
+        prefix: "slime",
+        suffix: ".png"
+    });
 
-    // var emitter = particles.createEmitter({
-    //     speed: 10,
-    //     scale: { start: 1, end: 0 },
-    //     blendMode: "ADD"
-    // });
+    scene.anims.create({
+        key: "walk",
+        frames: frameNames,
+        frameRate: 3,
+        repeat: -1
+    });
+    scene.slime.anims.play("walk");
+    scene.slime.setScale(0.5, 0.5);
+};
 
-    // var logo = this.physics.add.image(400, 100, "logo");
-
-    // logo.setVelocity(10, 300);
-    // logo.setBounce(1, 1);
-    // logo.setCollideWorldBounds(true);
-
-    // emitter.startFollow(logo);
-
-    const chick = this.add.sprite(64, 64, "atlas", "budbrain_chick.png");
-
-    const cop = this.add.sprite(600, 64, "atlas", "ladycop.png");
-
-    const robot = this.add.sprite(50, 300, "atlas", "robot.png");
-
-    const car = this.add.sprite(100, 400, "atlas", "supercars_parsec.png");
-
-    const mech = this.add.sprite(250, 100, "atlas", "titan_mech.png");
+function update(time, delta) {
+    this.slime.x -= delta / 8;
+    if (this.slime.x < 0) {
+        this.slime.x = 800;
+    }
 }
